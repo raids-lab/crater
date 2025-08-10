adbd3f605455b0cbbd1ba0a3aadfa2273f3f034d
liyilong <mondaycha@outlook.com>
2025-08-10 17:04:09 +0800

    refactor: update api baseurl

---
+++ hack/update-submodules-version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Updating submodules to the latest version..."
+
+echo "Updating crater-backend submodule..."
+cd crater-backend
+git fetch upstream
+git rebase upstream/main
+cd ..
+
+echo "Updating crater-frontend submodule..."
+cd crater-frontend
+git fetch upstream
+git rebase upstream/main
+cd ..
